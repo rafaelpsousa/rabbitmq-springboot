44ddae44fad07165ca924773702526ec0712359f
Rafael Sousa <rafaelps@unicamp.br>
2022-05-13 10:35:52 -0300

    first commit

---
+++ rabbitmq/src/main/java/br/unicamp/prefeitura/estoquepreco/RabbitmqApplication.java
@@ -0,0 +1,13 @@
+package br.unicamp.prefeitura.estoquepreco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqApplication.class, args);
+	}
+
+}
